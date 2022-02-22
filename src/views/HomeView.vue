35608c9044c22730e325e4f5f5a06092fee8e99d
Eunjong Kim <eunjong147@pusan.ac.kr>
2022-02-23 00:41:37 +0900

    login view background video

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <video src="../assets/star-background.mp4" muted autoplay loop></video>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-};
 </script>
